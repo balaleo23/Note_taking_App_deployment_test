14f18bd990b342b9b31f05cfaf5e82ac3804feaa
bala-linux <balamurugandamodaran@gmail.com>
2026-07-11 00:27:48 +0530

    allowed url got updated

--- quicknotes/settings.py
+++ quicknotes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1by(d8p2ixx0s915q&tj2s7+m4&il9kc@o5!ix()t9ry5$qv*r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','https://simple-deployment-app.onrender.com/']
 
 
 # Application definition
